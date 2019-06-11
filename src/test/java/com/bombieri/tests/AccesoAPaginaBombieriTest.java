930266849de1f5158b9b8d6145601c7d991b86e2
Bombieri5 <Bombieri5@Bombieri-WebDev1>
2019-06-11 10:07:09 -0300

    actualizacion de prueba de acceso a bombieri

--- src/test/java/com/bombieri/tests/AccesoAPaginaBombieriTest.java
+++ src/test/java/com/bombieri/tests/AccesoAPaginaBombieriTest.java
@@ -7,12 +7,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
 public class AccesoAPaginaBombieriTest {
 	
 	WebDriver driver;
+	WebDriverWait wait;
 	
 	@Test
 	public void test() {
@@ -20,6 +23,7 @@ public class AccesoAPaginaBombieriTest {
 		driver.findElement(By.name("q")).sendKeys("bombieri");
 		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
 		driver.findElement(By.className("LC20lb")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("custom-collapse")));
 		Assert.assertTrue(driver.findElement(By.id("custom-collapse")).isDisplayed());
 	}
   
@@ -27,6 +31,7 @@ public class AccesoAPaginaBombieriTest {
 	public void beforeTest() {
 		System.getProperties().setProperty("webdriver.chrome.driver", "src\\test\\resources\\WebDrivers\\chromedriver.exe");
 		driver = new ChromeDriver();
+		wait = new WebDriverWait(driver,30);
 	}
 
 	@AfterTest
